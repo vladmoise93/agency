5e32d09fb41c720ee71f2461895f1ab362587e1b
Vlad Moise <vladmoise93>
2020-02-18 13:23:51 +0200

    Updates

---
+++ precache-manifest.a75947c835993069eff8521705b7ea3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65d838f2b8d925627b63c7c0f901aa5",
+    "url": "/project2/index.html"
+  },
+  {
+    "revision": "8dba2f2fe742f889e59e",
+    "url": "/project2/static/css/main.12641f10.chunk.css"
+  },
+  {
+    "revision": "482f2d1a0e4c0ee52151",
+    "url": "/project2/static/js/2.5ea41002.chunk.js"
+  },
+  {
+    "revision": "10017bfb08c3f52cf1c70a46d09c7c96",
+    "url": "/project2/static/js/2.5ea41002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dba2f2fe742f889e59e",
+    "url": "/project2/static/js/main.60b8ecb7.chunk.js"
+  },
+  {
+    "revision": "cfbf28d0ec1269c74cff",
+    "url": "/project2/static/js/runtime-main.4d9b012b.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/project2/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/project2/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/project2/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/project2/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/project2/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
